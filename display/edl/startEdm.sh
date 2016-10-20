a62d98db65853b793cf4823117832d48b9df15b5
Jesus Vasquez <jvasquez@slac.stanford.edu>
2016-10-19 20:48:12 -0700

    Fix some bugs on YAML and EDL files

--- display/edl/startEdm.sh
+++ display/edl/startEdm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-edm -x -m "P=221-ATCA1" trigger.edl &
-edm -x -m "P=221-ATCA1" buffers.edl &
-edm -x -m "P=221-ATCA1" attenuators.edl &
-edm -x -m "P=221-ATCA1" dacs.edl &
+edm -x -m "P=yamlIOC1" trigger.edl &
+edm -x -m "P=yamlIOC1" buffers.edl &
+edm -x -m "P=yamlIOC1" attenuators.edl &
+edm -x -m "P=yamlIOC1" dacs.edl &
+edm -x -m "P=yamlIOC1" defaults.edl &
