65bad8646754279e41ca1f763831bb94959c7580
Jesus Vasquez <jvasquez@slac.stanford.edu>
2016-10-21 14:12:28 -0700

    Improve EDM screen for defaults and add one with FPGA info

--- display/edl/startEdm.sh
+++ display/edl/startEdm.sh
@@ -4,3 +4,4 @@ edm -x -m "P=yamlIOC1" buffers.edl &
 edm -x -m "P=yamlIOC1" attenuators.edl &
 edm -x -m "P=yamlIOC1" dacs.edl &
 edm -x -m "P=yamlIOC1" defaults.edl &
+edm -x -m "P=yamlIOC1" info.edl &
