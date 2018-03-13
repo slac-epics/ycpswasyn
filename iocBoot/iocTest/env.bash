d82fbce9c70f3d667260a48b750e5f0a0c2b6740
Jesus Vasquez <jvasquez@slac.stanford.edu>
2018-03-12 20:36:28 -0700

    Remove test IOC

--- iocBoot/iocTest/env.bash
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=/afs/slac/g/lcls/package/cpsw/framework/master/buildroot-2015.02-x86_64/lib/:/afs/slac/g/lcls/package/boost/1.57.0/buildroot-2015.02-x86_64/lib/:/afs/slac/g/lcls/package/yaml-cpp/yaml-cpp-0.5.3/buildroot-2015.02-x86_64/lib/:$LD_LIBRARY_PATH
